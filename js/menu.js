d9b3529bd14d9c578f6aa4c02f9c28b84ef08367
bruveping <bruveping@gmail.com>
2019-05-27 14:10:23 -0300

    hice varios cambios, agregue iconos y cambie menus, algo anda mal con el responsive

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+/*menu js*/
+
+
+let btnmenu = document.getElementById('btnmenu');
+let menu = document.getElementById('menu');
+btnmenu.addEventListener('click',function cambiaDeClase (){
+    'use strict';
+    menu.classList.toggle('mostrar');
+});
+
